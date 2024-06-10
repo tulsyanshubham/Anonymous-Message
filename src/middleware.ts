9f92b2863e2ec057762efc80eb4af2a8c22105ed
tulsyanshubham <141614975+tulsyanshubham@users.noreply.github.com>
2024-06-10 21:55:28 +0530

    login connected to frontend

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextResponse, NextRequest } from 'next/server'
+export { default } from "next-auth/middleware"
+import { getToken } from "next-auth/jwt"
+
+export async function middleware(request: NextRequest) {
+    const token = await getToken({ req: request })
+    const url = request.nextUrl
+
+    if (token && (
+        url.pathname.startsWith('/sign-in') ||
+        url.pathname.startsWith('/sign-up') ||
+        url.pathname.startsWith('/verify') ||
+        url.pathname.startsWith('/')
+    )) {
+        return NextResponse.redirect(new URL('/dashbard', request.url))
+    }
+    if(!token && url.pathname.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/sign-in', request.url))
+    }
+    return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: [
+        '/sign-up',
+        '/sign-in',
+        '/',
+        '/dashboard/:path*',
+        '/verify/:path*',
+    ]
+}
