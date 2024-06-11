b12f328d8b144416a23cb26031ff6f683d2829e8
tulsyanshubham <141614975+tulsyanshubham@users.noreply.github.com>
2024-06-11 14:43:05 +0530

    accept message status

--- src/app/api/accept-message/route.ts
+++ src/app/api/accept-message/route.ts
@@ -5,5 +5,77 @@ import UserModel from "@/model/user";
 import { User } from "next-auth";
 
 export async function POST(req: Request) {
-    
+    await dbConnect();
+
+    const session = await getServerSession(AuthOptions)
+    if (!session || !session.user) {
+        return Response.json({
+            success: false,
+            message: "Not Authenticated"
+        }, { status: 401 })
+    }
+    // const user : User = session?.user as User;
+    const user: User = session?.user;
+    const userId = user._id;
+    const { acceptMessages } = await req.json();
+
+    try {
+        const updatedUser = await UserModel.findByIdAndUpdate(userId, { isAcceptingMessages: acceptMessages }, { new: true });
+        if (!updatedUser) {
+            return Response.json({
+                success: false,
+                message: "Failed to update user status to accept messages"
+            }, { status: 401 })
+        }
+        return Response.json({
+            success: true,
+            message: "User status updated to accept messages",
+            updatedUser
+        }, { status: 200 })
+    } catch (error) {
+        console.log("Failed to update user status to accept messages")
+        return Response.json({
+            success: false,
+            message: "Failed to update user status to accept messages"
+        }, { status: 500 })
+    }
+}
+
+export async function GET(req: Request) {
+    await dbConnect();
+
+    const session = await getServerSession(AuthOptions)
+    if (!session || !session.user) {
+        return Response.json({
+            success: false,
+            message: "Not Authenticated"
+        }, { status: 401 })
+    }
+    const user: User = session?.user;
+    const userId = user._id;
+
+    try {
+
+        const foundUser = await UserModel.findById(userId);
+        if (!foundUser) {
+            return Response.json({
+                success: false,
+                message: "User not found"
+            }, { status: 404 })
+        }
+
+        return Response.json({
+            success: true,
+            message: "User found",
+            isAcceptingMessages: foundUser.isAcceptingMessages
+        }, { status: 200 })
+
+    } catch (error) {
+
+        console.log("Failed to find user")
+        return Response.json({
+            success: false,
+            message: "Failed to find user"
+        }, { status: 500 })
+    }
 }
