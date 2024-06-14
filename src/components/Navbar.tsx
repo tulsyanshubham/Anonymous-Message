406986f33ee2c7bc137dd68b6a5dbd52a575cc77
tulsyanshubham <141614975+tulsyanshubham@users.noreply.github.com>
2024-06-15 00:33:25 +0530

    few styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,20 +12,20 @@ export default function Navbar() {
     // const user: User = session?.user as User;
 
     return (
-        <nav className='p-4 md:p-6 shadow-md bg-gray-900 fixed w-full'>
-            <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
-                <a className='text-xl lg:text-3xl md:text-2xl font-bold mb-4 md:mb-0' href="/">
+        <nav className='px-2 py-4 md:p-6 shadow-md bg-gray-900 fixed w-full'>
+            <div className='container mx-auto flex flex-row justify-between items-center'>
+                <a className='text-ms lg:text-3xl md:text-2xl font-bold md:mb-0' href="/">
                     Anonymous Messages {session ? (`(${user.username || user.email})`) : ""}
                 </a>
                 {
                     session ? (
                         <>
                             {/* <span className='mr-4 text-xl font-bold'>Welcome, {user.username || user.email} </span> */}
-                            <Button className='w-full md:w-auto' onClick={() => signOut()}>Logout</Button>
+                            <Button className='w-fit md:w-auto' onClick={() => signOut()}>Logout</Button>
                         </>
                     ) : (
                         <Link href="/sign-in">
-                            <Button className="w-full md:w-auto bg-slate-100 text-black">Login</Button>
+                            <Button className="w-fit md:w-auto bg-slate-100 text-black">Login</Button>
                         </Link>
                     )
                 }
